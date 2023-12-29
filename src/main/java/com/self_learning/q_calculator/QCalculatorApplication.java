474b61ef7b1b7673eac306ab517fa381daef7070
fazilshafibhat <fazilshafi.fsb382@gmail.com>
2023-12-29 16:35:52 +0530

    initialize git

---
+++ src/main/java/com/self_learning/q_calculator/QCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.self_learning.q_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/self_learning/q_calculator/QCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self_learning.q_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
